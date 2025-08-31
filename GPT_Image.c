b9cba3fab33b9ab05bb2bc6e4fc90da6f4bf354d
RDXUzair <dexuzairsiddiqui441@gmail.com>
2025-08-31 08:13:40 +0530

    add file to esp

--- GPT_Image.c
+++ GPT_Image.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <time.h>
 #include <uchar.h>
+#include <string.h>
+#include <inttypes.h>
 
 typedef struct{
     uint32_t time_lo;
@@ -59,6 +61,92 @@ typedef struct{
         char16_t name[36];      //UCS-2
     } __attribute__((packed)) GPT_Partition_Entry;
 
+typedef struct{
+    uint8_t  BS_jmpBoot[3];
+    uint8_t  BS_OEMName[8];
+    uint16_t BPB_BytsPerSec;
+    uint8_t  BPB_SecPerClus;
+    uint16_t BPB_RsvdSecCnt;
+    uint8_t  BPB_NumFATs;
+    uint16_t BPB_RootEntCnt;
+    uint16_t BPB_TotSec16;
+    uint8_t  BPB_Media;
+    uint16_t BPB_FATSz16;
+    uint16_t BPB_SecPerTrk;
+    uint16_t BPB_NumHeads;
+    uint32_t BPB_HiddSec;
+    uint32_t BPB_TotSec32;
+    uint32_t BPB_FATSz32;
+    uint16_t BPB_ExtFlags;
+    uint16_t BPB_FSVer;
+    uint32_t BPB_RootClus;
+    uint16_t BPB_FSInfo;
+    uint16_t BPB_BkBootSec;
+    uint8_t  BPB_Reserved[12];
+    uint8_t  BS_DrvNum;
+    uint8_t  BS_Reserved1;
+    uint8_t  BS_BootSig;
+    uint8_t  BS_VolID[4];
+    uint8_t  BS_VolLab[11];
+    uint8_t  BS_FilSysType[8];
+
+    uint8_t  boot_code[510-90];  // Bootloader stub (legacy BIOS)
+    uint16_t bootsector_sig;     // 0xAA55
+} __attribute__((packed)) VBR;
+
+    typedef struct{
+        uint32_t FSI_LeadSig;
+        uint8_t  FSI_Reserved1[480];
+        uint32_t FSI_StrucSig;
+        uint32_t FSI_Free_Count;
+        uint32_t FSI_Nxt_Free;
+        uint8_t  FSI_Reserved2[12];
+        uint32_t FSI_TrailSig;
+    } __attribute__ ((packed)) FSInfo;
+
+typedef struct {
+    uint8_t  DIR_Name[11];
+    uint8_t  DIR_Attr;
+    uint8_t  DIR_NTRes;
+    uint8_t  DIR_CrtTimeTenth;
+    uint16_t DIR_CrtTime;
+    uint16_t DIR_CrtDate;
+    uint16_t DIR_LstAccDate;
+    uint16_t DIR_FstClusHI;
+    uint16_t DIR_WrtTime;
+    uint16_t DIR_WrtDate;
+    uint16_t DIR_FstClusLO;
+    uint32_t DIR_FileSize;
+} __attribute__ ((packed)) FAT32_DIR_ENTRY;
+
+    typedef enum{
+        ATTR_READ_ONLY = 0x01,
+        ATTR_HIDDEN    = 0x02,
+        ATTR_SYSTEM    = 0x04,
+        ATTR_VOLUME_ID = 0x08,
+        ATTR_DIRECTORY = 0x10,
+        ATTR_ARCHIVE   = 0x20,
+        ATTR_LONG_NAME = ATTR_READ_ONLY | ATTR_HIDDEN |
+                         ATTR_SYSTEM    | ATTR_VOLUME_ID       
+    }FAT32_DIR_ATTR;
+
+    typedef enum{
+        TYPE_DIR,
+        TYPE_FILE
+    }File_Type;
+
+typedef struct{
+    char        *image_name;
+    uint32_t    lba_size;
+    char        **esp_file_paths;
+    uint32_t    num_esp_file_paths;
+    char        **data_files;
+    uint32_t    num_data_files;
+    bool        vhd;
+    bool        help;
+    bool        error;
+}Options;
+
 const GUID ESP_GUID = {0xC12A7328, 0xF81F, 0x11D2, 0xBA, 0x4B,
     {0x00, 0xA0, 0xC9, 0x3E, 0xC9, 0x3B}};
 
@@ -69,17 +157,19 @@ enum{
     GPT_TABLE_ENTRY_SIZE = 128,
     NUMBER_OF_GPT_TABLE_ENTRIES = 128,
     GPT_TABLE_SIZE = 16384,     //As per UEFI Spec
-    ALIGNMENT = 1048576
+ALIGNMENT = 1048576
 };
 
-char *image_name = "Boot_Image";
+char *image_name = "BOOT_IMAGE.img";
 
     uint64_t lba_size = 512;
     uint64_t esp_size = 1024*1024*33;
     uint64_t data_size = 1024*1024*1;
     uint64_t image_size = 0;
-    uint64_t esp_size_lbas = 0, data_size_lbas = 0, image_size_lbas = 0;
-    uint64_t align_lba = 0, esp_lba = 0, data_lba = 0;
+    uint64_t esp_size_lbas = 0, data_size_lbas = 0, image_size_lbas = 0,
+             gpt_table_lbas = 0;
+    uint64_t align_lba = 0, esp_lba = 0, data_lba = 0,
+             fat32_fats_lba = 0, fat32_data_lba = 0;
 
 inline uint64_t bytes_to_lbas(const uint64_t bytes){
     return (bytes / lba_size) + (bytes % lba_size > 0 ? 1 : 0);
@@ -103,7 +193,7 @@ GUID new_guid(void){
             rand_arr[i] = rand() % (UINT8_MAX + 1) ;
         }
     
-        GUID result = {
+     GUID result = {
             .time_lo = *(uint32_t *)&rand_arr[0],
             .time_mid = *(uint16_t *)&rand_arr[4],
             .time_hi_and_ver = *(uint16_t *)&rand_arr[6],
@@ -158,6 +248,17 @@ uint32_t calculate_crc32(void *buf, int32_t len){
     return c ^ 0xFFFFFFFFL;
 }
 
+void get_fat_dir_entry_time_date(uint16_t *in_time, uint16_t *in_date){
+    time_t curr_time;
+    curr_time = time(NULL);
+
+        struct tm tm  = *localtime(&curr_time);
+        *in_date = ((tm.tm_year - 80) << 9) | ((tm.tm_mon + 1) << 5) | tm.tm_mday;
+
+        if(tm.tm_sec == 60) tm.tm_sec = 59;
+        *in_time = tm.tm_hour << 11 | tm.tm_min << 5 | (tm.tm_sec / 2);
+}
+
 bool write_protective_mbr(FILE *image){
     uint64_t mbr_image_lbas = image_size_lbas;
     if(mbr_image_lbas > 0xFFFFFFFF) mbr_image_lbas = 0x100000000;
@@ -194,8 +295,8 @@ bool write_gpt(FILE *image){
         .reserved_1 = 0,
         .my_lba = 1,
         .alternate_lba = image_size_lbas - 1,
-        .first_usable_lba = 1 + 1 + 32,
-        .last_usable_lba = image_size_lbas - 1 - 32 - 1,
+        .first_usable_lba = 1 + 1 + gpt_table_lbas,
+        .last_usable_lba = image_size_lbas - 1 - gpt_table_lbas - 1,
         .disk_guid = new_guid(),
         .partition_table_lba = 2,
         .number_of_entries = 128,
@@ -241,7 +342,7 @@ bool write_gpt(FILE *image){
             secondary_gpt.partition_table_crc32 = 0;
             secondary_gpt.my_lba = primary_gpt.alternate_lba;
             secondary_gpt.alternate_lba = primary_gpt.my_lba;
-            secondary_gpt.partition_table_lba = image_size_lbas - 1 - 32;
+            secondary_gpt.partition_table_lba = image_size_lbas - 1 - gpt_table_lbas;
 
 
         secondary_gpt.partition_table_crc32 = calculate_crc32(GPT_Table, sizeof(GPT_Table));
@@ -260,15 +361,362 @@ bool write_gpt(FILE *image){
     return true;
 }
 
-int main(void){
+bool write_esp(FILE *image){
+    const uint8_t reserved_sectors = 32;
+
+VBR vbr = {
+    .BS_jmpBoot = { 0xEB, 0x00, 0x90 },
+    .BS_OEMName = { "NAS OS  "},
+    .BPB_BytsPerSec = lba_size,
+    .BPB_SecPerClus = 1,
+    .BPB_RsvdSecCnt = reserved_sectors,
+    .BPB_NumFATs = 2,
+    .BPB_RootEntCnt = 0,
+    .BPB_TotSec16 = 0,
+    .BPB_Media = 0xF8,
+    .BPB_FATSz16 = 0,
+    .BPB_SecPerTrk = 0,
+    .BPB_NumHeads =0,
+    .BPB_HiddSec = esp_lba - 1,
+    .BPB_TotSec32 = esp_size_lbas,
+    .BPB_FATSz32 = (align_lba - reserved_sectors) / 2,
+    .BPB_ExtFlags = 0,
+    .BPB_FSVer = 0,
+    .BPB_RootClus = 2,
+    .BPB_FSInfo = 1,
+    .BPB_BkBootSec = 6,
+    .BPB_Reserved = { 0 },
+    .BS_DrvNum = 0x80,
+    .BS_Reserved1 = 0,
+    .BS_BootSig = 0x29,
+    .BS_VolID = { 0 },
+    .BS_VolLab = { "FAT32 VOL  "},
+    .BS_FilSysType = {"FAT32   "},
+
+    .boot_code = { 0 },
+    .bootsector_sig = 0xAA55
+};
+
+    FSInfo fsinfo = {
+        .FSI_LeadSig = 0x41615252,
+        .FSI_Reserved1 = { 0 },
+        .FSI_StrucSig = 0x61417272,
+        .FSI_Free_Count = 0xFFFFFFFF,
+        .FSI_Nxt_Free = 5,
+        .FSI_Reserved2 = { 0 },
+        .FSI_TrailSig = 0xAA550000
+    };
+
+fat32_fats_lba = esp_lba + vbr.BPB_RsvdSecCnt;
+fat32_data_lba = fat32_fats_lba + (vbr.BPB_NumFATs * vbr.BPB_FATSz32);
+
+    fseek(image, esp_lba * lba_size, SEEK_SET);
+
+        if(fwrite(&vbr, 1, sizeof(vbr), image) != sizeof(vbr)){
+            fprintf(stderr,"Error: could not write _vbr_ to disk\n");
+            return false;
+        }
+
+    padding_lba(image);
+
+    if(fwrite(&fsinfo, 1, sizeof(fsinfo), image) != sizeof(fsinfo)){
+        fprintf(stderr,"Error: could not write FAT32 info sector to image\n");
+        return false;
+    }
+
+    padding_lba(image);
+
+    fseek(image, (esp_lba + vbr.BPB_BkBootSec) * lba_size, SEEK_SET);  
+     
+        if(fwrite(&vbr, 1, sizeof(vbr), image) != sizeof(vbr)){
+            fprintf(stderr,"Error: could not write _vbr_ to disk\n");
+            return false;
+        }
+    
+    padding_lba(image); 
+
+    
+    if(fwrite(&fsinfo, 1, sizeof(fsinfo), image) != sizeof(fsinfo)){
+        fprintf(stderr,"Error: could not write FAT32 info sector to image\n");
+        return false;
+    }
+
+    padding_lba(image);
+
+        for(uint8_t i = 0; i < vbr.BPB_NumFATs; i++){
+            fseek(image, (fat32_fats_lba + (i * vbr.BPB_FATSz32)) * lba_size, SEEK_SET); 
+
+            uint32_t cluster = 0;
+
+            cluster = 0xFFFFFF00 | vbr.BPB_Media;
+            fwrite(&cluster, sizeof(cluster), 1, image);
+
+            cluster = 0xFFFFFFFF;
+            fwrite(&cluster, sizeof(cluster), 1, image);
+
+            cluster = 0xFFFFFFFF;
+            fwrite(&cluster, sizeof(cluster), 1, image);
+
+            cluster = 0xFFFFFFFF;
+            fwrite(&cluster, sizeof(cluster), 1, image);
+
+            cluster = 0xFFFFFFFF;
+            fwrite(&cluster, sizeof(cluster), 1, image);
+        }
+
+    fseek(image, fat32_data_lba * lba_size, SEEK_SET);
+
+        FAT32_DIR_ENTRY dir_ent = {
+            .DIR_Name = { "EFI        "},
+            .DIR_Attr = ATTR_DIRECTORY,             
+            .DIR_NTRes = 0,
+            .DIR_CrtTimeTenth = 0,
+            .DIR_CrtTime = 0,
+            .DIR_CrtDate = 0,
+            .DIR_LstAccDate = 0,
+            .DIR_FstClusHI = 0,
+            .DIR_WrtTime = 0,
+            .DIR_WrtDate = 0,
+            .DIR_FstClusLO = 3,
+            .DIR_FileSize = 0,
+        };
+
+    uint16_t create_time = 0, create_date = 0;
+    get_fat_dir_entry_time_date(&create_time, &create_date);
+
+    dir_ent.DIR_CrtTime = create_time;
+    dir_ent.DIR_CrtDate = create_date;
+    dir_ent.DIR_WrtTime = create_time;
+    dir_ent.DIR_WrtDate = create_date;
+
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    fseek(image, (fat32_data_lba + 1) * lba_size, SEEK_SET);
+    memcpy(dir_ent.DIR_Name, ".          ", 11); 
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    memcpy(dir_ent.DIR_Name, "..         ", 11);
+    dir_ent.DIR_FstClusLO = 0;
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    memcpy(dir_ent.DIR_Name, "BOOT       ", 11);
+    dir_ent.DIR_FstClusLO = 4;
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    fseek(image, (fat32_data_lba + 2) * lba_size, SEEK_SET); 
+    memcpy(dir_ent.DIR_Name, ".          ", 11); 
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    memcpy(dir_ent.DIR_Name, "..         ", 11);
+    dir_ent.DIR_FstClusLO = 3;
+    fwrite(&dir_ent, sizeof(dir_ent), 1, image);
+
+    return true;
+}
+
+bool add_file_to_esp(char *file_name, FILE *image, File_Type type, uint32_t *parent_dir_cluster) {
+    VBR vbr = { 0 };
+    fseek(image, esp_lba * lba_size, SEEK_SET);
+    fread(&vbr, sizeof vbr, 1, image);
+
+    FSInfo fsinfo = { 0 };
+    fseek(image, (esp_lba + 1) * lba_size, SEEK_SET);
+    fread(&fsinfo, sizeof fsinfo, 1, image);
+
+    FILE *new_file = NULL;
+    uint64_t file_size_bytes = 0, file_size_lbas = 0;
+    if (type == TYPE_FILE) {
+        new_file = fopen(file_name, "rb");
+        if (!new_file) return false;
+
+        fseek(new_file, 0, SEEK_END);
+        file_size_bytes = ftell(new_file);
+        file_size_lbas = bytes_to_lbas(file_size_bytes);
+        rewind(new_file);
+    }
+
+    uint32_t next_free_cluster = fsinfo.FSI_Nxt_Free;
+    const uint32_t starting_cluster = next_free_cluster;
+
+    for (uint8_t i = 0; i < vbr.BPB_NumFATs; i++) {
+        fseek(image, (fat32_fats_lba + (i * vbr.BPB_FATSz32)) * lba_size, SEEK_SET);
+        fseek(image, next_free_cluster * sizeof next_free_cluster, SEEK_CUR);
+
+        uint32_t cluster = fsinfo.FSI_Nxt_Free;
+        next_free_cluster = cluster;
+        if (type == TYPE_FILE) {
+            for (uint64_t lba = 0; lba < file_size_lbas - 1; lba++) {
+                cluster++;
+                next_free_cluster++;
+                fwrite(&cluster, sizeof cluster, 1, image);
+            }
+        }
+
+        cluster = 0xFFFFFFFF;
+        next_free_cluster++;
+        fwrite(&cluster, sizeof cluster, 1, image);
+    }
+
+    fsinfo.FSI_Nxt_Free = next_free_cluster;
+    fseek(image, (esp_lba + 1) * lba_size, SEEK_SET);
+    fwrite(&fsinfo, sizeof fsinfo, 1, image); 
+
+    fseek(image, (fat32_data_lba + *parent_dir_cluster - 2) * lba_size, SEEK_SET);
+
+    FAT32_DIR_ENTRY dir_entry = { 0 };
+
+    fread(&dir_entry, 1, sizeof dir_entry, image);
+    while (dir_entry.DIR_Name[0] != '\0')
+        fread(&dir_entry, 1, sizeof dir_entry, image);
+
+    fseek(image, -32, SEEK_CUR);    
+
+    const char *dot_pos = strchr(file_name, '.');
+    const uint32_t name_len = strlen(file_name);
+    if ((!dot_pos && name_len > 11) || 
+        (dot_pos && name_len > 12)  || 
+        (dot_pos && dot_pos - file_name > 8)) {
+        return false;
+    }
+    
+    memset(dir_entry.DIR_Name, ' ', 11);
+
+    if (dot_pos) {
+        uint8_t i = 0;
+        for (i = 0; i < (dot_pos - file_name); i++)
+            dir_entry.DIR_Name[i] = file_name[i];
+
+        uint8_t j = i;
+        while (i < 8) dir_entry.DIR_Name[i++] = ' ';
+
+        if (file_name[j] == '.') j++;
+
+        while (file_name[j])
+            dir_entry.DIR_Name[i++] = file_name[j++];
+
+        while (i < 11) dir_entry.DIR_Name[i++] = ' ';
+    } else {
+        memcpy(dir_entry.DIR_Name, file_name, name_len);
+    }
+
+    if (type == TYPE_DIR) 
+        dir_entry.DIR_Attr = ATTR_DIRECTORY;
+
+    uint16_t fat_time, fat_date;
+    get_fat_dir_entry_time_date(&fat_time, &fat_date);
+    dir_entry.DIR_CrtTime = fat_time;
+    dir_entry.DIR_CrtDate = fat_date;
+    dir_entry.DIR_WrtTime = fat_time;
+    dir_entry.DIR_WrtDate = fat_date;
+
+    dir_entry.DIR_FstClusHI = (starting_cluster >> 16) & 0xFFFF;
+    dir_entry.DIR_FstClusLO = starting_cluster & 0xFFFF;
+
+    if (type == TYPE_FILE)
+        dir_entry.DIR_FileSize = file_size_bytes;
+
+    fwrite(&dir_entry, 1, sizeof dir_entry, image);
+
+    fseek(image, (fat32_data_lba + starting_cluster - 2) * lba_size, SEEK_SET);
+
+    if (type == TYPE_DIR) {
+        memcpy(dir_entry.DIR_Name, ".          ", 11);
+        fwrite(&dir_entry, 1, sizeof dir_entry, image);
+
+        memcpy(dir_entry.DIR_Name, "..         ", 11);
+        dir_entry.DIR_FstClusHI = (*parent_dir_cluster >> 16) & 0xFFFF;
+        dir_entry.DIR_FstClusLO = *parent_dir_cluster & 0xFFFF;
+        fwrite(&dir_entry, 1, sizeof dir_entry, image);
+    } else {
+        uint8_t *file_buf = calloc(1, lba_size);
+        for (uint64_t i = 0; i < file_size_lbas; i++) {
+            size_t bytes_read = fread(file_buf, 1, lba_size, new_file);
+            fwrite(file_buf, 1, bytes_read, image);
+        }
+        free(file_buf);
+    }
+
+    if (type == TYPE_DIR)
+        *parent_dir_cluster = starting_cluster;
+
+    return true;
+}
+
+bool add_path_to_esp(char *path, FILE *image){
+    if (*path != '/') return false;
+
+        File_Type type = TYPE_DIR;
+        char *start = path + 1;
+        char *end = start;
+        uint32_t dir_cluster = 2;
+
+while (type == TYPE_DIR) {
+    while (*end != '/' && *end != '\0') end++;
+
+    if (*end == '/') type = TYPE_DIR;
+    else             type = TYPE_FILE;
+
+    *end = '\0';
+
+    FAT32_DIR_ENTRY dir_entry = { 0 };
+    bool found = false;
+    fseek(image, (fat32_data_lba + dir_cluster - 2) * lba_size, SEEK_SET);
+    do {
+        fread(&dir_entry, 1, sizeof dir_entry, image);
+        if (!memcmp(dir_entry.DIR_Name, start, strlen(start))) {
+            dir_cluster = (dir_entry.DIR_FstClusHI << 16) | dir_entry.DIR_FstClusLO;
+            found = true;
+            break;
+        }
+    } while (dir_entry.DIR_Name[0] != '\0');
+
+    if (!found) {
+        if (!add_file_to_esp(start, image, type, &dir_cluster))
+            return false;
+    }
+
+        *end++ = '/';
+        start = end;
+    }
+
+        printf("Added '%s'\n", path);
+
+    return true;        
+}
+
+bool add_disk_image_info_file(FILE *image) {
+    char *file_buf = calloc(1, lba_size);
+    snprintf(file_buf, 
+             lba_size,
+             "DISK_SIZE=%"PRIu64"\n", 
+             image_size);
+
+    FILE *fp = fopen("DSKIMG.INF", "wb");
+    if (!fp) return false;
+
+    fwrite(file_buf, strlen(file_buf), 1, fp);
+    fclose(fp);
+    free(file_buf);
+
+    char path[25] = { 0 };
+    strcpy(path, "/EFI/BOOT/DSKIMG.INF");
+    if (!add_path_to_esp(path, image)) return false;
+
+    return true;
+}
+
+int main(int argc, char *argv[]){
 
     FILE *image = fopen(image_name, "wb+");
     if(!image){
             fprintf(stderr, "Error: could not open file %s\n", image_name);
         return EXIT_FAILURE;
     }
+
+    FILE *file_ptr = NULL;
         
-    const uint64_t padding = (ALIGNMENT * 2 + (lba_size * 67));    
+    gpt_table_lbas = GPT_TABLE_SIZE / lba_size;
+    const uint64_t padding = (ALIGNMENT * 2 + (lba_size * ((gpt_table_lbas * 2) + 1 + 2)));    
 
         image_size = esp_size + data_size + padding;
         image_size_lbas = bytes_to_lbas(image_size);
@@ -285,13 +733,39 @@ int main(void){
 
         if(!write_protective_mbr(image)){
             fprintf(stderr, "Error: could not write Protective MBR to the disk");
+            fclose(image);
             return EXIT_FAILURE; 
         }
         
         if(!write_gpt(image)){
             fprintf(stderr, "Error: could not write GPT Table to the disk");
+            fclose(image);
             return EXIT_FAILURE;
         }
+
+        if(!write_esp(image)){
+            fprintf(stderr, "Error: could not write ESP to the disk");
+            fclose(image);
+            return EXIT_FAILURE;
+        }
+
+    file_ptr = fopen("BOOTX64.EFI", "rb");
+        if(file_ptr){
+            fclose(file_ptr);
+
+                char *path = calloc(1, 25);
+                strcpy(path, "/EFI/BOOT/BOOTX64.EFI");
+            
+            if(!add_path_to_esp(path, image)){
+                fprintf(stderr, "Error: could add file to the path '%s'\n", path);
+                free(path);
+            }
+        }
+
+         if (!add_disk_image_info_file(image)) 
+        fprintf(stderr, "Error: Could not add disk image info file to '%s'\n", image_name);
+
+    fclose(image);
 
     return EXIT_SUCCESS;
 }
